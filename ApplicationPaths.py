55aacbee69aa83e7b98a71444e0dc916d4ebb9a8
Conwenu <143044723+Conwenu@users.noreply.github.com>
2024-02-25 21:20:12 -0600

    Create ApplicationPaths.py
    Added the Applications Paths File

---
+++ ApplicationPaths.py
@@ -0,0 +1,4 @@
+ApplicationPaths = {
+    # We NEED to make is so that the user can update this and also make it so that we have multiple names for the same path.
+    "NotePad" : "C:/WINDOWS/system32/Notepad.exe",
+}
